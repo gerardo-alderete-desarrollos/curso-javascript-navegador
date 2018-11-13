cfcb57974a688e057844af6cfafaf9a1b3df425b
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-12 19:44:08 -0600

    CONTENIDO
    Modificaciones de la clase 6 ( contenido)

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,30 +1,12 @@
-/* const title = document.getElementById('title')
-const title2 = document.getElementById('title2')
-
-console.log(title.getAttribute('class'))
-console.log(title.getAttribute('id'))
-console.log(title.getAttribute('value'))
-
-if( title2 ){
-    title2.style.background = 'yellow'
-}
-console.log('Hola mundo')
-
- */
-
-//  const title = document.querySelector('h1')
-
-//  title.setAttribute('id','title')
-//  console.log(title)
-
-// title.id = 'title'
-// console.log(title)
-
 const title = document.querySelector('h1')
-// console.log(title.classList)
-title.classList.add('main-title', 'title-front-page')
-title.classList.remove('title-front-page')
 
+/* console.log(title.textContent)
+console.log(title.innerHTML)
+console.log(title.outerHTML) */
+
+
+// title.textContent = `Hola mama, <em> estoy en clase </em>`
+title.innerHTML = `Hola mama, <em> estoy en clase </em>`
 
 
 
