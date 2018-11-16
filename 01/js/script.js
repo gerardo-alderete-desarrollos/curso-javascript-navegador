e0f39d495955464f8b50294baade9b3945a67ecd
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 15:51:02 -0600

    EJERCICIO CON INSERTBEFORE
    Ejercicio realizado en la clase ejercicion con insertbefore

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,11 +1,29 @@
-const parent = document.getElementById('parent'),
-      childs = Array.from(parent.children),
-      childParent = childs.filter( child => child.children.length > 0)[0],
-      grandSons = Array.from(childParent.children),
-      grandSon3 = grandSons.filter( child => child.textContent.trim()
-        === 'Nieto 3')[0]
+// const parent = document.getElementById('parent'),
+//       childs = Array.from(parent.children),
+//       childParent = childs.filter( child => child.children.length > 0)[0],
+//       grandSons = Array.from(childParent.children),
+//       grandSon3 = grandSons.filter( child => child.textContent.trim()
+//         === 'Nieto 3')[0]
 
-const newElement = document.createElement('h2')
-newElement.textContent = 'Soy un nuevo elemento'
+// const newElement = document.createElement('h2')
+// newElement.textContent = 'Soy un nuevo elemento'
 
-childParent.insertBefore(newElement,grandSon3)
+// childParent.insertBefore(newElement,grandSon3)
+
+const post = document.getElementById('post')
+const ad = document.createElement('div')
+ad.id = 'edteam'
+ad.textContent = 'Publicidad'
+
+const postParagraphs = post.querySelectorAll('p')
+
+const getMiddleChild = element => {
+    const childs = element.children,
+          l = childs.length,
+          i = Math.floor(l / 2)
+
+    return childs[i]
+}
+ 
+const middleChild = getMiddleChild(post)
+post.insertBefore(ad, middleChild)
