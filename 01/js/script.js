a032d9c68ea39b39d3d281ff2f0054b51678a509
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 17:15:33 -0600

    CREAR ELEMENTOS Y FRAGMENTOS
    Ejercicio realizado en la clase crear elementos y fragmentos

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,11 +1,25 @@
-const parent = document.getElementById('parent')
-const ad = document.createElement('div')
+// const parent = document.getElementById('parent')
+// const ad = document.createElement('div')
 
-ad.id = 'edteam'
-ad.textContent = 'Publicidad'
+// ad.id = 'edteam'
+// ad.textContent = 'Publicidad'
 
-// parent.before(ad)
-// parent.after(ad)
-// parent.prepend(ad)
-// parent.append(ad)
-// parent.children[0].replaceWith(ad)
+// // parent.before(ad)
+// // parent.after(ad)
+// // parent.prepend(ad)
+// // parent.append(ad)
+// // parent.children[0].replaceWith(ad)
+
+const teachers = [ 'Alexys', 'Beto', 'Alvaro','Daniel', 'Jon']
+const teacherList = document.createElement('ul')
+const teacherListContainer = document.getElementById('teacher-list')
+const teacherListFragment = document.createDocumentFragment()
+teacherListContainer.appendChild(teacherList)
+
+for (const teacher of teachers) {
+    const li = document.createElement('li')
+    teacherListFragment.appendChild(li)
+    li.textContent = teacher
+}
+
+teacherList.appendChild(teacherListFragment)
