fb9e0ba8e40e82cbb9408d6fda7c5cc8d6ad228f
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 01:54:38 -0600

    EVENTOS DE TECLADO
    Fin de clase eventos de teclado

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,24 +1,54 @@
+// const input = document.getElementById('input')
 
-const createMenu = e => {
-    const menu = document.createElement('div')
-    const prevMenu = document.getElementById('context-menu')
-    menu.id = 'context-menu'
-    menu.textContent = 'Soy un menu contextual'
-    
-    if(prevMenu) document.body.removeChild(prevMenu)
-    
-    document.body.appendChild(menu)
+// addEventListener('keydown', e =>{
+//     if( e.key === 'a' && e.ctrlKey === true ){
+//         e.preventDefault()
+//         alert('CTRL A')
+//     }
+// })
+let x = 0, y = 0
+addEventListener('keyup', e => {
+    const ball = document.getElementById('ball')
+   
+    const move = (direction) => {
+        switch (direction) {
+            case 'up':
+                y--
+                break
+            case 'down':
+                y++
+                break
+            case 'left':
+                x--
+                break
+            case 'right':
+                x++
+                break
+            default:
+                break
 
-    menu.style.padding = '1em'
-    menu.style.background = '#eee'
-    menu.style.position = 'fixed'
-    menu.style.top = `${e.pageY}px`
-    menu.style.left = `${e.pageX}px`
+        }
 
-}
+        ball.style.transform = `translate(${x*10}px,${y*10}px)`
+    }
 
-document.addEventListener('contextmenu', e => {
-    createMenu(e)
-    e.preventDefault()
+
+
+    switch (e.key) {
+        case 'ArrowUp':
+            move('up')
+            break;
+        case 'ArrowDown':
+            move('down')
+            break;
+        case 'ArrowLeft':
+            move('left')
+            break;
+        case 'ArrowRight':
+            move('right')
+            break;
+        default:
+            break;
+    }        
 })
 
