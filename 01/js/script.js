231e10bdfcefd30b6a67f741b1e3892f56f0a3f5
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 15:39:23 -0600

    INSERTBEFORE()
    Ejercicio realizado en la clase insert before

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,9 +1,11 @@
-const parent = document.getElementById('parent')
-const grandsond = document.getElementById('grandson-2')
+const parent = document.getElementById('parent'),
+      childs = Array.from(parent.children),
+      childParent = childs.filter( child => child.children.length > 0)[0],
+      grandSons = Array.from(childParent.children),
+      grandSon3 = grandSons.filter( child => child.textContent.trim()
+        === 'Nieto 3')[0]
 
-// console.log(parent.firstElementChild)
+const newElement = document.createElement('h2')
+newElement.textContent = 'Soy un nuevo elemento'
 
-// console.log(grandsond.nextElementSibling)
-
-console.log(grandsond.parentElement)
-
+childParent.insertBefore(newElement,grandSon3)
