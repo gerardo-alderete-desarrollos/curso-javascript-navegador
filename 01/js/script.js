37fba362ca5590044d13508fc9eef1ce38e8855e
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 15:15:30 -0600

    DOM TRAVERSING
    Ejemplo realizado en la clase dom traversing

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,9 +1,9 @@
-const gallery = document.getElementById('gallery')
+const parent = document.getElementById('parent')
+const grandsond = document.getElementById('grandson-2')
 
-gallery.addEventListener('click', e=> {
-    const t = e.target
-    const images = Array.from(gallery.querySelectorAll('img'))
-    const i = images.indexOf(t)
+// console.log(parent.firstElementChild)
 
-    console.log(`Hiciste click en la imagen ${i + 1}`)
-})
+// console.log(grandsond.nextElementSibling)
+
+console.log(grandsond.parentElement)
+
