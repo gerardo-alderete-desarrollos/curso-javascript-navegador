c91cb8addffa70908b7476a3ff69112eb56c00c4
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-15 22:41:11 -0600

    EVENTOS MULTIMEDIA
    Ejemplo realizado en la clase eventos multimedia

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,32 +1,12 @@
-// addEventListener('DOMContentloaded', () => {
-//     const form = document.getElementById('form')
-//     const rememberPassword = document.getElementById('remember-password')
-
-//     form.addEventListener('submit', (e) => {
-//         console.log('Ha enviado el formulario')
-//     })
-
-//     rememberPassword.addEventListener('change', (e) => {
-//         if( e.target.cheched ){
-//             console.log('El usuario quiere recordar su contraseña')
-//         }else{
-//             console.log('El usuario NO quiere recordar su contraseña')
-
-//         }
-//     })
-// })
-
-
-// addEventListener('scroll', e => {
-//     console.log(scrollX, scrollY)
-// })
-
-addEventListener('resize', e => {
-    console.log(e.target.innerWidth, e.target.innerHeight)
-    console.log(innerWidth, innerHeight)
-
-    console.log(e.target.outerWidth, e.target.outerHeight)
-    console.log(outerWidth, outerHeight)
-
+const video = document.getElementById('video'),
+      playVideo = document.getElementById('play'),
+      pauseVideo = document.getElementById('pause')
     
+playVideo.addEventListener('click', () => {
+    video.play()
+})
+
+pauseVideo.addEventListener('click', () => {
+    // video.pause()
+    playVideo.click()
 })
