4da6a0f84368f18db3163633253cec08ca0edf3a
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-15 23:06:45 -0600

    PROPAGACION DE EVENTOS
    Ejemplo realizado en la clase propagacion de eventos

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,12 +1,20 @@
-const video = document.getElementById('video'),
-      playVideo = document.getElementById('play'),
-      pauseVideo = document.getElementById('pause')
+// const video = document.getElementById('video'),
+//       playVideo = document.getElementById('play'),
+//       pauseVideo = document.getElementById('pause')
     
-playVideo.addEventListener('click', () => {
-    video.play()
-})
+// playVideo.addEventListener('click', () => {
+//     video.play()
+// })
 
-pauseVideo.addEventListener('click', () => {
-    // video.pause()
-    playVideo.click()
+// pauseVideo.addEventListener('click', () => {
+//     // video.pause()
+//     playVideo.click()
+// })
+
+document.querySelectorAll('div').forEach( el => {
+    el.addEventListener('click', (e) => {
+        console.log(`Hiciste click en el div ${e.target.id}`)
+        e.stopPropagation()
+        
+    })
 })
