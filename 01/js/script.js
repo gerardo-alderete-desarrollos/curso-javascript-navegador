5325afbb555c7a5284ecb181fd9c5ce66574d24e
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 16:08:44 -0600

    REMPLAZAR HIJOS
    Ejemplo realizado en la clase remplazar hijos

--- 01/js/script.js
+++ 01/js/script.js
@@ -4,17 +4,8 @@ const ad = document.createElement('div')
 ad.id = 'edteam'
 ad.textContent = 'Publicidad'
 
-// parent.insertAdjacentElement('beforebegin', ad)
-// parent.insertAdjacentElement('afterend', ad)
-// parent.insertAdjacentElement('afterbegin', ad)
-parent.insertAdjacentElement('beforeend', ad)
+parent.replaceChild(ad, parent.children[0])
 
-// parent.insertAdjacentText('beforeend', 'Hola mundo')
-// parent.insertAdjacentText('beforebegin', 'Hola mundo')
-
-
-// parent.insertAdjacentHTML('beforebegin', `<h1>Soy el titulo
-    // principal</h1>`)
 
 
 
