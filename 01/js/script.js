e9f5626af2ec5af8b53f3273170818de4f6fb3ab
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-15 22:22:54 -0600

    EVETNOS DEL DOM Y NAVEGADOR
    Ejemplos realizados en la clase eventos del dom y navegador

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,15 +1,32 @@
-const form = document.getElementById('form')
-const rememberPassword = document.getElementById('remember-password')
+// addEventListener('DOMContentloaded', () => {
+//     const form = document.getElementById('form')
+//     const rememberPassword = document.getElementById('remember-password')
 
-form.addEventListener('submit', (e) => {
-    console.log('Ha enviado el formulario')
-})
+//     form.addEventListener('submit', (e) => {
+//         console.log('Ha enviado el formulario')
+//     })
 
-rememberPassword.addEventListener('change', (e) => {
-    if( e.target.cheched ){
-        console.log('El usuario quiere recordar su contraseña')
-    }else{
-        console.log('El usuario NO quiere recordar su contraseña')
+//     rememberPassword.addEventListener('change', (e) => {
+//         if( e.target.cheched ){
+//             console.log('El usuario quiere recordar su contraseña')
+//         }else{
+//             console.log('El usuario NO quiere recordar su contraseña')
 
-    }
+//         }
+//     })
+// })
+
+
+// addEventListener('scroll', e => {
+//     console.log(scrollX, scrollY)
+// })
+
+addEventListener('resize', e => {
+    console.log(e.target.innerWidth, e.target.innerHeight)
+    console.log(innerWidth, innerHeight)
+
+    console.log(e.target.outerWidth, e.target.outerHeight)
+    console.log(outerWidth, outerHeight)
+
+    
 })
