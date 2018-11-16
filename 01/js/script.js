7843acc5bc2236b0dbd8cf7d6121b12fe6070ced
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-16 16:54:31 -0600

    METODOS JQUERY
    Ejercicio realizado en la clase metodos jquery en javascript puro

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,15 +1,11 @@
-
 const parent = document.getElementById('parent')
 const ad = document.createElement('div')
+
 ad.id = 'edteam'
 ad.textContent = 'Publicidad'
 
-parent.replaceChild(ad, parent.children[0])
-
-
-
-
-
-
-
-
+// parent.before(ad)
+// parent.after(ad)
+// parent.prepend(ad)
+// parent.append(ad)
+// parent.children[0].replaceWith(ad)
